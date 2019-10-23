c1632d51996b1314e06ef5af237418564ac992cd
Nadya Moisseeva <nmoisseeva@eos.ubc.ca>
2019-10-23 12:13:42 -0700

    auto-generate namelists

---
+++ init/generate_namelist.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+#=======input==========
+InputFile=namelist.input
+Spinup="spinup"
+EditVal="fire_fuel_cat      = "
+LoopVar=(2 3 4 5 6 7 8 9 10 11 12 13)
+BaseVal=1
+#======end of input====
+
+current=$(pwd)
+echo "Creating namelist set for $EditVal:"
+cd ./namelist/
+for i in "${LoopVar[@]}"
+do
+	#move to folder
+	run=${InputFile}F${i}$Spinup
+	echo "Editing run: $run"
+	cp ${InputFile}F$BaseVal$Spinup $run
+	sed -i "s/$EditVal$BaseVal/$EditVal$i/g" $run
+done
+cd $current
+echo "COMPLETE"
