5281947ede81cc05acc38b164cbdd5e23da568d5
Nadya Moisseeva <nmoisseeva@eos.ubc.ca>
2021-01-14 19:14:58 -0800

    attempt with new wrf-sfire

--- sample_run/spinup_run/submit_to_slurm_spinup.sh
+++ sample_run/spinup_run/submit_to_slurm_spinup.sh
@@ -3,10 +3,10 @@
 #SBATCH --mem-per-cpu=3000M
 #SBATCH --nodes=1
 #SBATCH --ntasks=48
-#SBATCH --account=def-rstull
+#SBATCH --account=rrg-rstull
 
-module load  nixpkgs/16.09  intel/2016.4  openmpi/2.1.1
-module load wrf-fire-1tracer
+#module load  nixpkgs/16.09  intel/2016.4  openmpi/2.1.1
+#module load wrf-fire-1tracer
 
-mpirun -np 1 ideal.exe
-srun wrf.exe
+mpirun -np 1 /home/moisseev/projects/rrg-rstull/moisseev/WRF-SFIRE/run/ideal.exe
+srun /home/moisseev/projects/rrg-rstull/moisseev/WRF-SFIRE/run/wrf.exe
